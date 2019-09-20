e4ef74a8071130c522c02e341155c7f1dfe74b25
victor politano <viant3ucf@gmail.com>
2019-09-20 05:52:53 -0400

    changed por in connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: process.env.PORT || 3000,
   user: "root",
   password: "1234",
   database: "burgers_db"
