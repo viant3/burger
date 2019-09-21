cf6fa4361fabb5f09fc0bd3189712d52685e4dcd
victor politano <viant3ucf@gmail.com>
2019-09-21 08:12:17 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 
+
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
+  user: "gdy66lhioyqwn1ju",
+  password: "wnlzecv2747fv21j",
+  database: "i11746drunz1928o"
 });
 
 connection.connect(function(err) {
